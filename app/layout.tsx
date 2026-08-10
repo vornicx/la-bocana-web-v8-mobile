567c4d25c28ebaa0912fe01e1e5a93ac47a7ebfa
vornicx <vornicx@gmail.com>
2026-08-10 10:42:49 +0200

    Aplica cierre compacto del footer móvil

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import './mobile-header.css';
 import './mobile-menu.css';
 import './mobile-layout.css';
 import './mobile-dock.css';
+import './mobile-footer-fix.css';
 import { headers } from 'next/headers';
 import { chromeCopy, type PublicLocale } from '@/lib/i18n';
 
